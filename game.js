94bb95fccd0463875888b2ac0fd89e9167fdcad6
AFeese <58886725+AFeese@users.noreply.github.com>
2021-07-30 23:51:15 -0400

    loaded sprites

---
+++ game.js
@@ -0,0 +1,29 @@
+kaboom({
+  global: true,
+  fullscreen: true,
+  scale: 1,
+  debug: true,
+  clearColor: [0, 0, 0, 1],
+});
+
+loadRoot("https://imgur.com/");
+loadSprite("coin", "wbKxhcd.png");
+loadSprite("evil-shroom", "KPO3fR9.png");
+loadSprite("brick", "pogC9x5.png");
+loadSprite("block", "bdrLpi6.png");
+loadSprite("mario", "Wb1qfhK.png");
+loadSprite("mushroom", "0wMd92p.png");
+loadSprite("suprise", "gesQ1KP.png");
+loadSprite("unboxed", "bdrLpi6.png");
+loadSprite("pipe-top-left", "ReTPiWY.png");
+loadSprite("pipe-top-right", "hj2GK4n.png");
+loadSprite("pipe-bottom-left", "c1cYSbt.png");
+loadSprite("pipe-bottom-right", "nqQ79eI.png");
+
+scene("game", () => {
+  layers(["bg", "obj", "ui"], "obj")
+
+  const map = 
+});
+
+start("game");
