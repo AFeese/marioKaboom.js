fa878e9f5e217d24da84c0d7bcec30146eafe5d5
AFeese <58886725+AFeese@users.noreply.github.com>
2021-08-01 20:43:56 -0400

    trying to debug 'start is not defined' error.

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+// const k = kaboom();
+
 kaboom({
   global: true,
   fullscreen: true,
@@ -6,23 +8,24 @@ kaboom({
   clearColor: [0, 0, 0, 1],
 });
 
-loadRoot("https://imgur.com/");
-loadSprite("coin", "wbKxhcd");
-loadSprite("evil-shroom", "KPO3fR9");
-loadSprite("brick", "pogC9x5");
-loadSprite("block", "bdrLpi6");
-loadSprite("mario", "Wb1qfhK");
-loadSprite("mushroom", "0wMd92p");
-loadSprite("suprise", "gesQ1KP");
-loadSprite("unboxed", "bdrLpi6");
-loadSprite("pipe-top-left", "ReTPiWY");
-loadSprite("pipe-top-right", "hj2GK4n");
-loadSprite("pipe-bottom-left", "c1cYSbt");
-loadSprite("pipe-bottom-right", "nqQ79eI");
+// import k from "./kaboom";
+
+loadRoot("https://i.imgur.com/");
+loadSprite("coin", "wbKxhcd.png");
+loadSprite("evil-shroom", "KPO3fR9.png");
+loadSprite("brick", "pogC9x5.png");
+loadSprite("block", "bdrLpi6.png");
+loadSprite("mario", "Wb1qfhK.png");
+loadSprite("mushroom", "0wMd92p.png");
+loadSprite("surprise", "gesQ1KP.png");
+loadSprite("unboxed", "bdrLpi6.png");
+loadSprite("pipe-top-left", "ReTPiWY.png");
+loadSprite("pipe-top-right", "hj2GK4n.png");
+loadSprite("pipe-bottom-left", "c1cYSbt.png");
+loadSprite("pipe-bottom-right", "nqQ79eI.png");
 
 scene("game", () => {
   layers(["bg", "obj", "ui"], "obj");
-
   const map = [
     "                                       ",
     "                                       ",
@@ -35,14 +38,13 @@ scene("game", () => {
     "                                       ",
     "===============================   =====",
   ];
-
   const levelCfg = {
     width: 20,
     height: 20,
     "=": [sprite("block", solid())],
   };
-
   const gameLevel = addLevel(map, levelCfg);
 });
 
 start("game");
+// console.log(start);
---
+++ kaboom.js
@@ -0,0 +1,11 @@
+// export const k = kaboom({
+//   //   global: true,
+//   //   fullscreen: true,
+//   width: 640,
+//   height: 480,
+//   scale: 1,
+//   debug: true,
+//   clearColor: [0, 0, 0, 1],
+// });
+
+// export default k;
