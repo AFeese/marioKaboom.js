42512e5b33be756de41dbbfc67b8dbd9601024c1
AFeese <58886725+AFeese@users.noreply.github.com>
2021-08-05 22:39:40 -0400

    added 'big' function for when mario consumes the mushroom.

--- game.js
+++ game.js
@@ -74,6 +74,35 @@ scene("game", () => {
 
   add([text("level" + "test", pos(4, 6))]);
 
+  function big() {
+    let timer = 0;
+    let isBig = false;
+    return {
+      update() {
+        if (isBig) {
+          //dt is Delta Time since last frame. Listed on the Kaboom.js docs.
+          timer -= dt();
+          if (timer <= 0) {
+            this.smallify();
+          }
+        }
+      },
+      isBig() {
+        return isBig;
+      },
+      smallify() {
+        this.scale = vec2(1);
+        timer = 0;
+        isBig = false;
+      },
+      biggify(time) {
+        this.scale = vec2(2);
+        timer = time;
+        isBig = true;
+      },
+    };
+  }
+
   const player = add([
     sprite("mario"),
     solid(),
